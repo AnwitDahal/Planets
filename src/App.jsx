1a6488a8b950350449d2264e2b59a98e4807e5b9
AnwitDahal <dahalanwit0707@gmail.com>
2024-09-08 17:25:03 +0545

    Earth

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import React from "react";
+import React from 'react'
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import "./style.css";
-import Scene from "./Scene";
-import {
-  Bloom,
-  EffectComposer,
-  ToneMapping,
-} from "@react-three/postprocessing";
+
+import Test from './Components/Test'
+import { OrbitControls, Stars } from '@react-three/drei';
+import './style.css'
 
 const App = () => {
   return (
-    <Canvas flat camera={{ fov: 85 }}>
-      <OrbitControls />
-      <ambientLight />
-      <Scene />
-      <EffectComposer>
-        <Bloom
-          mipmapBlur
-          intensity={12.0} // The bloom intensity.
-          luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
-          luminanceSmoothing={0} // smoothness of the luminance threshold. Range is [0, 1]
-        />
-        {/* <ToneMapping adaptive/> */}
-      </EffectComposer>
+    <Canvas>
+      <OrbitControls/>
+      <Stars 
+        radius={1}       // Radius of the sphere that holds the stars
+        depth={100}         // Star field depth (for parallax effect)
+        count={500}       // Number of stars
+        factor={4}         // Star size factor
+        saturation={0}     // Color saturation for stars
+        fade={true}        // Enable fading of stars at the edges
+      />
+      <ambientLight/>
+    <Test/>
+    {/* <Test2/> */}
     </Canvas>
-  );
-};
+    
+  )
+}
 
-export default App;
+export default App
---
+++ src/Components/Test.jsx
@@ -0,0 +1,37 @@
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Sphere, useTexture } from "@react-three/drei";
+import * as THREE from 'three'
+
+const Test = () => {
+    let tex=useTexture("./Images/8k_earth_daymap.jpg");
+    let cloudTexture=useTexture("./Images/8k_earth_clouds.jpg");
+    let planet=useRef()
+    let cloudRef=useRef()
+    useFrame((state,delta)=>{
+        planet.current.rotation.y+=delta*0.05
+        cloudRef.current.rotation.y+=delta*0.1  
+    })
+
+  return (
+    <>
+      <mesh ref={planet}>
+      <sphereGeometry args={[2,50,50]}/>
+        <meshStandardMaterial map={tex} 
+        roughness={0}/>
+        {/* <meshBasicMaterial color={0xffff}/> */}
+      </mesh>
+      <mesh ref={cloudRef}>
+        <sphereGeometry args={[2.02, 50, 50]} /> {/* Slightly larger radius */}
+        <meshStandardMaterial
+          map={cloudTexture}
+          transparent={true}   // Make sure the clouds are transparent
+          opacity={0.8}   
+          blending={THREE.AdditiveBlending}  // Adjust opacity as needed
+        />
+      </mesh>
+    </>
+  )
+}
+
+export default Test
