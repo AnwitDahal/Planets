c2835708c599f2e53572c882cd894c3ac6e4ba0f
AnwitDahal <dahalanwit0707@gmail.com>
2024-09-12 01:57:27 +0545

    Prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LandingPage from './Components/LandingPage'
+import SimpleSlider from './Components/SliderComponent'
 
 const App = () => {
   return (
     <>
-    <LandingPage/>
+    <LandingPage/>  
+    {/* <SimpleSlider/> */}
     </>
   )
 }
--- src/Components/Saturn.jsx
+++ src/Components/Saturn.jsx
@@ -1,22 +1,36 @@
 import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Sphere, useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 const Saturn = () => {
-  let tex = useTexture("./Images/8k_saturn.jpg");
-  let planet = useRef();
-  let cloudRef = useRef();
+  // Load textures for planet and ring
+  let planetTexture = useTexture("./Images/8k_saturn.jpg");
+  let ringTexture = useTexture("./Images/8k_saturn_ring_alpha.png"); // Ensure this is a transparent texture for the ring
+
+  // Refs for planet and ring
+  let planetRef = useRef();
+  let ringRef = useRef();
+
+  // Rotation animation for the planet
   useFrame((state, delta) => {
-    planet.current.rotation.y += delta * 0.05;
+    planetRef.current.rotation.y += delta * 0.05;
+    ringRef.current.rotation.x = Math.PI / 2; // Tilt the ring slightly
+    ringRef.current.rotation.z += delta * 0.02; // Rotate the ring slowly
   });
 
   return (
     <>
-      <mesh ref={planet}>
+      {/* Saturn Planet */}
+      <mesh ref={planetRef}>
         <sphereGeometry args={[2, 50, 50]} />
-        <meshStandardMaterial map={tex} roughness={0} />
-        {/* <meshBasicMaterial color={0xffff}/> */}
+        <meshStandardMaterial map={planetTexture} roughness={0} />
+      </mesh>
+
+      {/* Saturn Ring */}
+      <mesh ref={ringRef} rotation={[Math.PI / 2, 0, 0]}>
+        <ringGeometry args={[2.5, 2, 64]} /> {/* Inner radius (2.5), outer radius (4) */}
+        <meshBasicMaterial map={ringTexture} side={THREE.DoubleSide} transparent={true} />
       </mesh>
     </>
   );
