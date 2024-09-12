8ea2c694beaef293ccf26e713abc9a63e9f28c47
AnwitDahal <dahalanwit0707@gmail.com>
2024-09-12 20:08:48 +0545

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import LandingPage from './Components/LandingPage'
-import SimpleSlider from './Components/SliderComponent'
 
 const App = () => {
   return (
     <>
     <LandingPage/>  
-    {/* <SimpleSlider/> */}
     </>
   )
 }
